f1a77aadee192e4a07d54955f25a1ca7595ec4ea
unknown <windunc@gmail.com>
2022-01-13 15:41:52 +0700

    Gravatar dan Bcrypt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -31,10 +33,31 @@ router.post(
       if (user) {
         res.status(400).json({ errors: [{ msg: 'User already exists' }] });
       }
+
       // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
       // Encrypt password
-      // Return jsonwebtoken
-      res.send('Users route');
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user
+        .save()
+
+        // Return jsonwebtoken
+        .res.send('Users Registered');
     } catch (error) {
       console.log(error.message);
       res.status(500).send('Server Error');
