939b9b05d05f21f59d158d60e2d422fd94199739
unknown <windunc@gmail.com>
2022-01-13 15:30:49 +0700

    POST and Check Validator Express

--- config/db.js
+++ config/db.js
@@ -3,16 +3,17 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            UseNewUrlParser: true
-        });
-        console.log("MonggoDB Connected");
-    } catch (error) {
-        console.log(error.message);
-        // exit process with failur
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(db, {
+      UseNewUrlParser: true,
+      //   useCreateIndex: true,
+    });
+    console.log('MonggoDB Connected');
+  } catch (error) {
+    console.log(error.message);
+    // exit process with failur
+    process.exit(1);
+  }
+};
 
-module.exports = connectDB; 
+module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,13 +2,44 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
+const User = require('../../models/User');
+
 // @route   POST api/Users
 // @desc    Register
 // @access  Public
-router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('Users route')
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
+    const { name, email, password } = req.body;
+
+    try {
+      // See if users exists
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // Get users gravatar
+      // Encrypt password
+      // Return jsonwebtoken
+      res.send('Users route');
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
